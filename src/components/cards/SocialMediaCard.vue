37bb6e14b69e0d4fca7eaf98b0ec85196218af54
joebarker <joebarker@mail.weber.edu>
2021-03-16 16:12:58 -0600

    first commit

---
+++ src/components/cards/SocialMediaCard.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="social-media-card">
+
+    <div class="social-media-card__top">
+      <div class="social-media-card__social-image">
+        <slot name="svg"></slot>
+      </div>
+      <div class="social-media-card__name">
+        <p><slot name="name"></slot></p>
+      </div>
+    </div>
+
+    <div class="social-media-card__middle">
+      <div class="social-media-card__media-followers-count">
+        <p>
+          <slot name="followers-count"></slot>
+        </p>
+      </div>
+      <div class="social-media-card__media-followers-text">
+        <p>
+          <slot name="description"></slot>
+        </p>
+      </div>
+    </div>
+
+    <div class="social-media-card__bottom">
+      <div class="social-media-card__arrow">
+        <slot name="arrow"></slot>
+      </div>
+      <slot name="today"></slot>
+    </div>
+
+  </div>
+
+</template>
+
+<script>
+export default {
+name: "SocialMediaCard"
+}
+</script>
+
+<style scoped>
+
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,15 @@
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  css: {
+    loaderOptions: {
+      // by default the `sass` option will apply to both syntaxes
+      // because `scss` syntax is also processed by sass-loader underlyingly
+      // but when configuring the `prependData` option
+      // `scss` syntax requires an semicolon at the end of a statement, while `sass` syntax requires none
+      // in that case, we can target the `scss` syntax separately using the `scss` option
+      scss: {
+        prependData: '@import "~@/sass/main.scss";'
+      }
+    }
+  }
 }
